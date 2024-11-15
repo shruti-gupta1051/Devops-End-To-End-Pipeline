efbe02d7f0884d941001cd097e3c57f3580970eb
shruti-gupta1051 <shruti.gupta1051@gmail.com>
2024-11-15 17:09:56 +0530

    Update var.tf

--- deploy/terraformscripts/var.tf
+++ deploy/terraformscripts/var.tf
@@ -1,11 +1,37 @@
-output "outputofamiId"{
-    value = data.aws_ami.latest_amazon_linux
+variable "instanceType"{
+type = string
+default = "t2.medium"
 }
 
-output "output1"{
-    value = var.x
+variable "x"{
+    type = string
+    default = "hello"
 }
 
-output "vpc_id"{
-    value = data.aws_vpcs.default_vpc
+variable "instanceTagName"{
+    type = string
+    default = "GFGTerraform"
+}
+
+variable "sg_name"{
+    default = "WebserverSg"
+}
+
+variable "allowedPort"{
+    type = list 
+    default     = [
+    6443,  # Kubernetes API server
+    2379,  # etcd server client API
+    2380,  # etcd server peer API
+    10250, # Kubelet API
+    10251, # kube-scheduler
+    10252, # kube-controller-manager
+    30000, # NodePort Services start
+    32767,  # NodePort Services end
+    80,
+    22,
+    8080,
+    3000, 
+    9090
+  ]
 }
